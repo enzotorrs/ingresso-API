f405a655f4a9df3a605b08f72504d7f56c3d5432
enzotorrs <enzotorres969@gmail.com>
2022-01-21 09:56:45 -0300

    adicionado view para servir filmes em alta

--- filmes/views.py
+++ filmes/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from . import models
-from .import serializer
+from . import serializer
+from . import scraping
 
 
 class FilmeViewSet(viewsets.ModelViewSet):
@@ -10,4 +11,17 @@ class FilmeViewSet(viewsets.ModelViewSet):
 
 
 def atualiza_banco(request):
+    filmes_em_alta = scraping.filmes_em_alta()
+    
+    models.Filme.objects.all().delete()
+
+    for filme in filmes_em_alta:
+        novo_filme = models.Filme(
+            nome_filme=filme['titulo'],
+            imagem_font=filme['img'],
+            em_alta= True,
+            em_cartaz=True,
+        )
+        novo_filme.save()
+
     return render(request, 'atualiza_banco.html')
